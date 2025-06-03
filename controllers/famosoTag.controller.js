c27d2998dcd1af91e1697ca96d32ce0a0e00dd49
Juan Sebastian Rios Altamirano <sebasriosalt@gmail.com>
2025-06-02 20:08:53 -0500

    Descripción breve del cambio realizado

--- controllers/famosoTag.controller.js
+++ controllers/famosoTag.controller.js
@@ -28,7 +28,7 @@ const obtenerTagsPorUsuario = async (req, res) => {
     try {
         const tags = await FamosoTag.find({ usuario: req.usuario._id })
             .populate('famoso', 'nombre');
-        res.json({ tags }); // <-- Cambiado para devolver un objeto con la propiedad tags
+        res.json({ tags }); // <-- Cambiado para devolver un objeto con la propiedad tagsasdada
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
